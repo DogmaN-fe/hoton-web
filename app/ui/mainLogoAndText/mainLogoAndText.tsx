898942cd62234e92008cc8b43d1e8b1a2ade0d80
Eduard Mednikov <EduardMedh@yandex.ru>
2024-03-23 16:26:32 +0300

    Фикс

--- app/ui/mainLogoAndText/mainLogoAndText.tsx
+++ app/ui/mainLogoAndText/mainLogoAndText.tsx
@@ -1,14 +1,14 @@
-import Image from "next/image";
 import mainLogo from "@/public/Logo.jpg";
 import styles from "./mainLogoAndText.module.css";
 import { ReactElement } from "react";
 import BookingButton from "../bookingButton/bookingButton";
+import ExportedImage from "next-image-export-optimizer";
 
 export default function MainLogoAndText(): ReactElement {
   return (
     <section className={styles.main_logo_section}>
       <div className={styles.main_logo_section_position}>
-        <Image
+        <ExportedImage 
           className={styles.main_logo_img}
           src={mainLogo}
           width={350}
