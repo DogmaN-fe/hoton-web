febbe1793baf383375684855378ea60efafdeb65
Eduard <126475818+DogmaN-fe@users.noreply.github.com>
2024-03-23 14:13:49 +0300

    Update page.tsx

--- app/reservations/page.tsx
+++ app/reservations/page.tsx
@@ -2,11 +2,8 @@
 
 import { ReactElement } from "react";
 import styles from "./page.module.css";
-import { useSearchParams } from "next/navigation";
 
 export default function Page(): ReactElement {
-  const router = useSearchParams();
-  const roomType = router.get("room-type");
   let address = `https://reservationsteps.ru/rooms/index/aea23486-a63a-4e1d-802c-0d24d26a0608`;
   
 
