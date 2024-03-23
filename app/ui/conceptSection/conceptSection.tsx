cce03bbc5c314d197a826f6a971a0884746dd1b3
Eduard Mednikov <EduardMedh@yandex.ru>
2024-03-23 20:57:28 +0300

    Удалена точка в концепции

--- app/ui/conceptSection/conceptSection.tsx
+++ app/ui/conceptSection/conceptSection.tsx
@@ -7,10 +7,10 @@ export default function ConceptSection(): ReactElement {
     <section id="concept" className={styles.concept}>
       <div className={styles.concept_block}>
         <h2 className={styles.concept_concept_description_title}>
-          Место силы и комфорта в окружении цветущей степи.{" "}
+          Место силы и комфорта в окружении цветущей степи
         </h2>
         <p className={styles.concept_description}>
-          Уникальное место для перезагрузки отдыха душой и телом
+          Уникальное место для перезагрузки отдыха душой и телом.
         </p>
         <BookingButton className={styles.concept_booking_button} />
       </div>
